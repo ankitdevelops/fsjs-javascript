995520c917e2b0f79fc61800f376d396fc833ba5
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-01-17 18:07:08 +0530

    -

---
+++ learn-js/01_Basics/condition.js
@@ -0,0 +1,51 @@
+// if else
+
+// syntax
+
+// if (condition) {
+//   // code to be executed when the condition is evaluated to truthy value
+// } else {
+//   // code to be executed when the condition is falsy value
+// }
+
+// let age = 16;
+
+// if (age >= 18) {
+//   console.log("You can vote");
+// } else if (age >= 16) {
+//   console.log("You are eligible for Driving License");
+// } else {
+//   console.log("You can't vote right now.");
+// }
+
+// switch
+let score = 75;
+
+// switch (x) {
+//   case "value1":
+//     //   code to be executed
+//     break;
+//   case "value2": // if (x === 'value2')
+//     //   code to be executed
+//     break;
+
+//   default:
+//     //   code to be executed
+//     break;
+// }
+
+let color = "green";
+
+switch (color) {
+  case "red":
+    console.log("The color is red.");
+    break;
+  case "green":
+    console.log("The color is green.");
+    break;
+  case "blue":
+    console.log("The color is blue.");
+    break;
+  default:
+    console.log("The color is not red, green, or blue.");
+}
--- learn-js/01_Basics/operator.js
+++ learn-js/01_Basics/operator.js
@@ -15,10 +15,60 @@
 // console.log(55 / 5);
 // console.log(56 % 5);
 
+// const var1 = 10;
+// const var2 = 20;
+// const var3 = 10;
+
+// // Greater than
+// console.log(var1 > var2); // false
+// console.log(var2 > var1); //true
+
+// // Less than
+// console.log(var1 < var2); // true
+// console.log(var2 < var1); // false
+
+// // Greater than or equal
+// console.log(var1 >= var2); // false
+// console.log(var2 >= var1); // true
+// console.log(var1 >= var3); // true
+
+// // Less than or equal
+// console.log(var1 <= var2); // true
+// console.log(var2 <= var1); // false
+// console.log(var1 <= var3); // true
+
+// Logical Operator
+
 const var1 = 10;
 const var2 = 20;
-const var3 = "10";
-const var4 = 10;
-console.log(var1 !== var2); // true
-console.log(var1 !== var3); // true
-console.log(var1 !== var4); // false
+const var3 = 10;
+
+// console.log(var1 > var2 || var2 > var1);
+// console.log(true || true); // true
+// console.log(false || true); // true
+// console.log(true || false); // true
+// console.log(false || false); // false
+
+// console.log(var1 > var2 && var2 > var1); // false &&
+// console.log(true && true); // true
+// console.log(false && true); // false
+// console.log(true && false); // false
+// console.log(false && false); // false
+
+// not
+// console.log(!true); //false
+// console.log(!false); //true
+// console.log(!0); //true
+// console.log(!"Hello"); // false
+
+// console.log(var2 > var1 ? "true" : "false"); // true
+
+// console.log(var1 > var2 ? "true" : "false"); // false
+
+let x = 5;
+console.log(x++); // Output: 5
+console.log(x); // Output: 6
+
+let y = 5;
+console.log(++y); // Output: 6
+console.log(y); // Output: 6
