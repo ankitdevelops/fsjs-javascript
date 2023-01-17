c8f0a11033d308be8d6c42b42e7a3e35d4728a4f
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-01-17 18:07:56 +0530

    question2

---
+++ assignment-js/08-Jan/Q2/index.js
@@ -0,0 +1,14 @@
+// Declare variables to store your first name, last name, marital status, country and age and display them using interpolation method.
+
+const firstName = "Ankit";
+const lastName = "Kumar";
+const isMarried = false;
+const country = "India";
+const age = 21;
+
+console.log(`
+Name: ${firstName} ${lastName}
+Marital Status: ${isMarried ? "Married" : "unmarried"}
+Country: ${country}
+Age: ${age}
+`);
