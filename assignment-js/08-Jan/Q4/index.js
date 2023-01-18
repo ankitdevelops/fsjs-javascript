f08ec81e5a5aa133804f08bb56f13ed19c501eab
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-01-18 20:04:08 +0530

    fourth question

---
+++ assignment-js/08-Jan/Q4/index.js
@@ -0,0 +1,5 @@
+// Declare a variable and assign string value to it and check if the string contains a word Script using includes() method
+
+const text = "The Script for the play was full of unexpected twists.";
+
+console.log(text.includes("Script")); // true
