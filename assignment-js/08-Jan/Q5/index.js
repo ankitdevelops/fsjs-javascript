2a7424edaabcd14cbdc6dcfb6dedd0fe62d85f33
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-01-19 21:22:37 +0530

    question 5

---
+++ assignment-js/08-Jan/Q5/index.js
@@ -0,0 +1,24 @@
+// Declare a variable and assign string value to it and then split it into an array using split() method
+
+const text = "The Script for the play was full of unexpected twists.";
+
+console.log(text.split(" "));
+// OutPut
+/*
+[
+  "The",
+  "Script",
+  "for",
+  "the",
+  "play",
+  "was",
+  "full",
+  "of",
+  "unexpected",
+  "twists.",
+];
+
+*/
+
+const string = "unexpected";
+console.log(string.split("")); // ["u", "n", "e", "x", "p", "e", "c", "t", "e", "d"];
