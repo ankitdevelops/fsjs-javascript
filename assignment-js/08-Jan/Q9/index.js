7869cc184961eb58a6e97be9e5a6c2cdeaed340c
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-01-21 06:54:18 +0530

    question 9

---
+++ assignment-js/08-Jan/Q9/index.js
@@ -0,0 +1,18 @@
+// Boolean value is either true or false.
+// - Write three JavaScript statement example which provide truthy value.
+// - Write three JavaScript statement example which provide falsy value.
+
+const var1 = 10;
+const var2 = 20;
+const var3 = "10";
+const var4 = 10;
+
+// Truthy Value
+console.log(var1 == var3); // true
+console.log(var1 == var4); // true
+console.log(var1 === var4); // true
+
+// Falsy Value
+console.log(var1 !== var4); // false
+console.log(var1 != var3); // false
+console.log(var1 != var4); //false
