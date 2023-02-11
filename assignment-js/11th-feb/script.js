494366c0046668b2409e2ab136354139a05d3d4b
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-02-11 23:04:58 +0530

    first-relese

--- assignment-js/11th-feb/script.js
+++ assignment-js/11th-feb/script.js
@@ -1,45 +1,77 @@
 const userInput = document.querySelector("#userInput");
-const temperature = document.querySelector("#temp");
-const temperatureImg = document.querySelector("#weatherImg");
-const forecastContainer = document.querySelectorAll("#forecast");
+const forecastContainer = document.querySelector("#forecast");
+const card = document.querySelector(".wether_condition");
 const getData = async (event) => {
-  if (!userInput.value) {
-    alert("Enter Your City Name");
-  }
+  let data;
   event.preventDefault();
-  const city = userInput.value ? userInput.value : "London";
+  const city = userInput.value ? userInput.value : "New Delhi";
   const fetchData = await fetch(
-    // `http://api.weatherapi.com/v1/current.json?key=b11a7755c1f247c2995112058231102&q=${city}&days=5&aqi=no&alerts=no`
     `http://api.weatherapi.com/v1/forecast.json?key=b11a7755c1f247c2995112058231102&q=${city}&days=5&aqi=no&alerts=no`
   );
-  const data = await fetchData.json();
+  if (!data) {
+    card.innerHTML = `<div class="loader"></div>`;
+    forecastContainer.innerHTML = "";
+  }
+  data = await fetchData.json();
+  console.log(data);
+
   if (!data.error) {
-    console.log(data);
-    temperature.innerText = data.current.temp_c;
-    temperatureImg.src = data.current.condition.icon;
-    console.log(data.forecast);
+    weatherConditon(data.current.temp_c, data.current.condition.icon);
     data.forecast.forecastday.forEach((forecastData) => {
-      console.log(forecastData);
-      createElement();
+      let icon = forecastData.day.condition.icon;
+      let temp = forecastData.day.avgtemp_c;
+      let date = forecastData.date;
+      userInput.setAttribute("placeholder", data.location.name);
+      forecastElement(icon, temp, date);
     });
   } else {
-    alert(data.error.message);
+    // alert(data.error.message);
+    card.innerHTML = `
+    <div>
+        <img
+        src="./img/404.svg"
+        alt="weather-condition-img"
+        class="block"
+        id="forecast_img"
+        />
+    </div>
+    <h6 class="text-xs" id="forecast_temp">${data.error.message}</h6>
+    `;
   }
+  userInput.value = "";
 };
 
-function createElement() {
-  const element = document.createAttribute("div");
+function forecastElement(icon, temp, date) {
+  const element = document.createElement("div");
+  element.classList.add("flex", "items-center", "flex-col");
   element.innerHTML = `
-  <div class="flex items-center flex-col" id="forecast">
-              <img
-                src="//cdn.weatherapi.com/weather/64x64/day/122.png"
-                alt="weather-condition-img"
-                class="h-3/5 w-3/5"
-                id="forecast_img"
-              />
-              <h6 class="text-xl" id="forecast_temp">34</h6>
-              <h6 class="text-xl" id="forecast_day">Thur</h6>
-            </div>
+    <img
+    src=${icon}
+    alt="weather-condition-img"
+    class=""
+    id="forecast_img"
+    />
+    <h6 class="text-xl text-white" id="forecast_temp ">${temp}</h6>
+   
   `;
-  forecastContainer.appendChild = element;
+
+  forecastContainer.appendChild(element);
 }
+
+function weatherConditon(temp, icon) {
+  card.innerHTML = `
+    <div class="condition__img w-1/2">
+        <img
+        src="${icon}"
+        alt="weather-condition-img"
+        id="weatherImg"
+        class="scale-[2]"
+        />
+    </div>
+    <div class="conditon__temp w-1/2">
+        <span class="text-7xl font-thin text-white" id="temp">${temp}</span>
+        <span class="absolute text-2xl font-medium text-white">.C</span>
+    </div> 
+    `;
+}
+// <h6 class="text-xs" id="forecast_day">${date}</h6>
